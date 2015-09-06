019606392e6cf5746adece6d6d6ff5797b6c18ad
Kris Nye <krisnye@gmail.com>
2015-09-05 21:23:13 -0700

    added sugar core classes

--- src/es6/String.js
+++ src/es6/String.js
@@ -1,28 +1,22 @@
-if (!String.prototype.endsWith) {
-    Object.defineProperty(String.prototype, 'endsWith', {
-        enumerable: false,
-        configurable: true,
-        writable: false,
-        value: function (searchString, position) {
-            position = position || this.length;
-            position = position - searchString.length;
-            var lastIndex = this.lastIndexOf(searchString);
-            return lastIndex !== -1 && lastIndex === position;
-        }
-    });
-}
+// if (!String.prototype.endsWith) {
+//     Object.defineProperty(String.prototype, 'endsWith', {
+//         value: function (searchString, position) {
+//             position = position || this.length;
+//             position = position - searchString.length;
+//             var lastIndex = this.lastIndexOf(searchString);
+//             return lastIndex !== -1 && lastIndex === position;
+//         }
+//     });
+// }
 
-if (!String.prototype.startsWith) {
-  Object.defineProperty(String.prototype, 'startsWith', {
-    enumerable: false,
-    configurable: true,
-    writable: false,
-    value: function (searchString, position) {
-      position = position || 0;
-      return this.indexOf(searchString, position) === position;
-    }
-  });
-}
+// if (!String.prototype.startsWith) {
+//   Object.defineProperty(String.prototype, 'startsWith', {
+//     value: function (searchString, position) {
+//       position = position || 0;
+//       return this.indexOf(searchString, position) === position;
+//     }
+//   });
+// }
 
 // deprecated in favor of includes
 if (!String.prototype.contains ) {
