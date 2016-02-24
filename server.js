e6e245892dbfd21da8cdbec67c6b012207b76871
Kris Nye <krisnye@gmail.com>
2016-02-24 11:36:00 -0700

    updated to fix all server samples

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
 #!/usr/bin/env node
 
+var fs = require('fs');
 var express = require('express');
 var server = express();
 var port = 9090;
 
+server.use("/js/ion", express.static("lib"));
+var manifest = JSON.parse(fs.readFileSync("lib/manifest.json"))
+var scripts = manifest.files.map(function(x){return "ion/" + x;});
+server.use("/scripts.js", function(req, res){
+    res.send("(" + JSON.stringify(scripts) + ".forEach(function(a) { document.writeln(\"<script src='/js/\" + a + \"'></script>\") }));");
+});
+
 server.use(express.static(__dirname + '/docs/www'));
-server.use("/bower_components/ion/lib", express.static(__dirname + '/lib'));
 
 server.listen(port, function() {
     console.log('Listening on port ' + port)
