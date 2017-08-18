af1b2365a348277971d6afcf41907b27db3ed8cc
KodyJKing <kody.j.king@gmail.com>
2017-08-18 16:38:19 -0700

    Improved windows watch script.

---
+++ watchWin.js
@@ -0,0 +1,10 @@
+const child_process = require('child_process')
+function exec(cmd) {
+    let proc = child_process.exec(cmd)
+    proc.stdout.on('data', (x) => process.stdout.write(x))
+    proc.stderr.on('data', (x) => process.stderr.write(x))
+    proc.on('exit', (code) => console.log('child process exited with code ' + code.toString()))
+}
+exec('nodemon -w src/parser.pegs -x pegs src/parser.pegs lib/parser.js')
+exec('tsc -p . --watch')
+exec('nodemon -w lib -w test -e js,ion lib/command.js test')
