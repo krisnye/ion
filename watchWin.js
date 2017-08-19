9152c21c44cb4a6448e9a6e6661748abcd4e19ed
KodyJKing <kody.j.king@gmail.com>
2017-08-18 19:37:19 -0700

    fixed win script

--- watchWin.js
+++ watchWin.js
@@ -1,9 +1,9 @@
+#!/usr/bin/env node
 const child_process = require('child_process')
 function exec(cmd) {
     let proc = child_process.exec(cmd)
     proc.stdout.on('data', (x) => process.stdout.write(x))
     proc.stderr.on('data', (x) => process.stderr.write(x))
-    proc.on('exit', (code) => console.log('child process exited with code ' + code.toString()))
 }
 exec('nodemon -w src/parser.pegs -x pegs src/parser.pegs lib/parser.js')
 exec('tsc -p . --watch')
