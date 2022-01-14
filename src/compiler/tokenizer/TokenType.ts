9e010db21cc9672e585ecf8f101caa5d979c3b44
Kris Nye <krisnye@gmail.com>
2022-01-13 18:19:38 -0800

    added tokenizer tests

--- src/compiler/tokenizer/TokenType.ts
+++ src/compiler/tokenizer/TokenType.ts
@@ -4,12 +4,13 @@ type ValueFunction = (source: string) => any;
 
 export class TokenType {
 
+    readonly name: string;
     readonly match: (line: string) => number;
     readonly value?: ValueFunction;
 
-    constructor(match: RegExp, value?: ValueFunction)
-    constructor(match: (line: string) => number, value?: ValueFunction)
-    constructor(match: RegExp | ((line: string) => number), value?: ValueFunction) {
+    constructor(name: string, match: RegExp, value?: ValueFunction)
+    constructor(name: string, match: (line: string) => number, value?: ValueFunction)
+    constructor(name: string, match: RegExp | ((line: string) => number), value?: ValueFunction) {
         if (match instanceof RegExp) {
             const regexp = match;
             match = (line: string) => {
@@ -17,6 +18,7 @@ export class TokenType {
                 return result != null ? result[0].length : -1;
             }
         }
+        this.name = name;
         this.match = match;
         this.value = value;
     }
@@ -24,15 +26,15 @@ export class TokenType {
 }
 
 export const tokenTypes = {
-    Id: new TokenType(/^[_@a-z][_$@a-z0-9]+/i),
-    Operator: new TokenType(/^[\=\+\-\*\&\^\%\#\!\~\/\.\:\;\?\,\<\>]+/i),
-    Tab: new TokenType(/^((    )|\t)+/, source => source.length / 4),
-    Whitespace: new TokenType(/^\s+/),
-    Open: new TokenType( /^[\({\[]/),
-    Close: new TokenType(/^[\)}\]]/),
-    Float: new TokenType(/^-?[1-9][0-9]*\.[0-9]+(e[+-]?[0-9]+)?/, JSON.parse),
-    Integer: new TokenType(/^-?([1-9][0-9]*|0x[0-9]+)/, JSON.parse),
-    String: new TokenType(/^"([^"\\]|\\.)*"/, JSON.parse),
-    Comment: new TokenType(/^\/\/.*/),
-    Unknown: new TokenType(/^.*/),
+    Id: new TokenType("Id", /^[_@a-z][_$@a-z0-9]*/i),
+    Comment: new TokenType("Comment", /^\/\/.*/),
+    Operator: new TokenType("Operator", /^[\=\+\-\*\&\^\%\#\!\~\/\.\:\;\?\,\<\>]+/i),
+    Tab: new TokenType("Tab", /^((    )|\t)+/, source => source.length / 4),
+    Whitespace: new TokenType("Whitespace", /^\s+/),
+    Open: new TokenType("Open",  /^[\({\[]/),
+    Close: new TokenType("Close", /^[\)}\]]/),
+    Float: new TokenType("Float", /^-?[1-9][0-9]*\.[0-9]+(e[+-]?[0-9]+)?/, JSON.parse),
+    Integer: new TokenType("Integer", /^-?([1-9][0-9]*|0x[0-9]+)/, JSON.parse),
+    String: new TokenType("String", /^"([^"\\]|\\.)*"/, JSON.parse),
+    Unknown: new TokenType("Unknown", /^.*/),
 } as const;
--- src/compiler/tokenizer/test/tokenizer.test.ts
+++ src/compiler/tokenizer/test/tokenizer.test.ts
@@ -1,11 +1,35 @@
 import { strict as assert } from "assert";
 import { Tokenizer } from "../Tokenizer";
+import { tokenTypes } from "../TokenType";
 
 let tokenizer = new Tokenizer();
 
-let lines = tokenizer.tokenizeFile("sample.ion" ,
-`
-        hello = () => foo
-`);
+function testLine(line: string, expectedResult: string) {
+    let { tokens } = tokenizer.tokenizeLine("test.ion", line, 1);
+    let actualResult = tokens.filter(token => token.type != tokenTypes.Whitespace.name).map(token => [token.type, token.value ?? token.source].join(":")).join("\n");
+    console.log("--------------------")
+    console.log(actualResult);
+    assert.equal(actualResult, expectedResult, line);
+}
 
-console.log(JSON.stringify(lines, null, "  "));
+testLine("f",
+`Id:f`);
+
+testLine("foo",
+`Id:foo`);
+
+testLine("    foo: bar = () => a.b // foo",
+`Tab:1
+Id:foo
+Operator::
+Id:bar
+Operator:=
+Open:(
+Close:)
+Operator:=>
+Id:a
+Operator:.
+Id:b
+Comment:// foo`);
+
+testLine(`"foo\\"bar"`, `String:foo"bar`)
